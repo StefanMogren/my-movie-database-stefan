311a1b23d7c3309f04f31d4c5e0d2cbf9513d460
StefanMogren <stefan_mogren@hotmail.com>
2025-02-18 09:42:07 +0100

    Added fetchAPI function and fetchTopMovies function

--- scripts/modules/api.js
+++ scripts/modules/api.js
@@ -1,7 +1,26 @@
-import oData from '../data/data.js';
+// import oData from '../data/data.js';
 
-export async function fetchTopMovies() {
+/* export async function fetchTopMovies() {
     const response = await fetch('https://santosnr6.github.io/Data/favoritemovies.json');
     let movies = await response.json();
     oData.topMovieList = movies;
+} */
+
+async function fetchAPI(api) {
+    // Får titta till errormeddelandet lite noggrannare senare
+    return fetch(api)
+        .then(response => response.json())
+        .catch(error => console.log(error.message))
+
+/*         const response = await fetch(api);
+        const data = await response.json();
+        return data */
+
 }
+
+async function fetchTopMovies() {
+    let movies = await fetchAPI("https://santosnr6.github.io/Data/favoritemovies.json");
+    return movies;
+}
+
+export { fetchTopMovies };
