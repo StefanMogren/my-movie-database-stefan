3446806efc037f1b757f61a01f2306821f5dd77b
StefanMogren <stefan_mogren@hotmail.com>
2025-02-18 11:46:33 +0100

    Created folder data/ with data.js, added const oData object to data.js, adjusted API call and movie functions to use oData

--- scripts/script.js
+++ scripts/script.js
@@ -2,49 +2,53 @@ import { apiKeyOMDb } from "../apiKeys/apiKeyOMDb.js"
 import { fetchTopMoviesAPI } from "./modules/api.js";
 import { renderTrailers } from "./modules/caroussel.js";
 import { randomizeNumbers } from "./utils/utils.js";
+import { oData } from "./data/data.js";
 
 if(window.location.pathname === '/' || window.location.pathname === '/index.html') {
     console.log('index.html');
-
+    
 } else if(window.location.pathname === '/favorites.html') {
     console.log('favorites.html');
-
+    
 } else if(window.location.pathname === '/movie.html') {
     console.log('movie.html');
-
+    
 } else if(window.location.pathname === '/search.html') {
     console.log('search.html');
-
+    
 }
+
+// oData.movies definieras samt får alla filmer från API:et
+await fetchTopMoviesAPI();
+
 
 
 pickMovieTrailers(5);
 
-async function pickMovieTrailers(amount) {
-    const movies = await pickMovies(amount);
+function pickMovieTrailers(amount) {
+    const movies = pickRandomMovies(amount);
     
     movies.forEach( (movie, i) => {
         renderTrailers(movie, (i + 1))
     })
 }
 
-async function pickMovies(amount) {
+function pickRandomMovies(amount) {
     const numbers = randomizeNumbers(amount);
-    const topMovies = await fetchTopMoviesAPI();
     const moviesArray = [];
-
+    
     numbers.forEach( number => {
-        moviesArray.push(topMovies[number]);
+        moviesArray.push(oData.movies[number]);
     })
-
+    
     return moviesArray;
 }
 
 
 pickTopMovies(20);
 
-async function pickTopMovies(amount) {
-    const movies = await pickMovies(amount)
-
+function pickTopMovies(amount) {
+    const movies = pickRandomMovies(amount)
+    
     console.log(movies)
 }
