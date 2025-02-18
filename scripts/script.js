70a4b0f480e63e381fee148fe75c30164eee18dc
StefanMogren <stefan_mogren@hotmail.com>
2025-02-18 10:37:50 +0100

    Added randomizeNumbers function to utils.js, added pickMovieTrailers function to script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 import { apiKeyOMDb } from "../apiKeys/apiKeyOMDb.js"
-
 import { fetchTopMovies } from "./modules/api.js";
+import { renderTrailers } from "./modules/caroussel.js";
+import { randomizeNumbers } from "./utils/utils.js";
 
 if(window.location.pathname === '/' || window.location.pathname === '/index.html') {
     console.log('index.html');
@@ -16,5 +17,19 @@ if(window.location.pathname === '/' || window.location.pathname === '/index.html
 
 }
 
-console.log(await fetchTopMovies());
+
+pickMovieTrailers();
+async function pickMovieTrailers() {
+    const numbers = randomizeNumbers(5);
+    const topMovies = await fetchTopMovies();
+    const fiveMovies = [];
+    
+    numbers.forEach( number => {
+        fiveMovies.push(topMovies[number]);
+    })
+
+    fiveMovies.forEach( (movie, i) => {
+        renderTrailers(movie, (i + 1))
+    })
+}
 
