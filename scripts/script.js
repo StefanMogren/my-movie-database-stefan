ab03aa75d8c8d87f26b3cbc6958300b1c38fb29f
StefanMogren <stefan_mogren@hotmail.com>
2025-02-28 09:53:15 +0100

    Moved URLSearchParams and associated codes from script.js to the runMoviePage function in moviePage.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,13 @@
-import { apiKeyOMDb } from "../apiKeys/apiKeyOMDb.js"
-import { fetchOMDbFullMovieAPI, searchOMDbMoviesAPI } from "./modules/api.js";
-import { oData } from "./data/data.js";
+import { searchOMDbMoviesAPI } from "./modules/api.js";
 import { runMoviePage } from "./components/moviePage.js";
 import { runFavoritesPage } from "./components/favoritesPage.js"
 import { runSearchPage } from "./components/searchPage.js";
+import { oData } from "./data/data.js";
 import { runIndexPage } from "./components/indexPage.js";
 
 // Om index.html-sidan laddas körs detta
 if(window.location.pathname === '/' || window.location.pathname === '/index.html') {
     console.log('index.html');
-    // oData.movies definieras samt får alla filmer från API:et när fetchTopMoviesAPI() körs
     runIndexPage();
     
     
@@ -21,20 +19,9 @@ if(window.location.pathname === '/' || window.location.pathname === '/index.html
     // Om movie.html laddas körs detta
 } else if(window.location.pathname === '/movie.html') {
     console.log('movie.html');
-    window.onload = async () => {
-        const urlParams = new URLSearchParams(window.location.search);
-        const imdbID = urlParams.get("imdbid")
-        
-        if(imdbID) {
-            console.log(`The IMDb ID is ${imdbID}`);
-            await fetchOMDbFullMovieAPI(apiKeyOMDb, imdbID)
-            console.log(oData.fullInfoMovie);
-            runMoviePage(oData.fullInfoMovie);
-            
-        } else {
-            console.error("No imdbID is present...");
-        }
-    }
+    runMoviePage();
+    
+    
     
     // Om search.html laddas körs detta
 } else if(window.location.pathname === '/search.html') {
@@ -42,14 +29,14 @@ if(window.location.pathname === '/' || window.location.pathname === '/index.html
         console.log('search.html');
         const urlParams = new URLSearchParams(window.location.search);
         oData.searchInput = urlParams.get("searchMovie")
-
+        
         if(oData.searchInput) {
             await searchOMDbMoviesAPI(apiKeyOMDb, oData.searchInput)
             runSearchPage();
         } else {
             console.error("No search input is present...");
         }
-
+        
     }
     
 }
