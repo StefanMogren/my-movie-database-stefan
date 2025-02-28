d1ff6c160b46e805dd969b4edc74710003f2dc6f
StefanMogren <stefan_mogren@hotmail.com>
2025-02-28 15:07:07 +0100

    Removed unneeded console.log()

--- scripts/script.js
+++ scripts/script.js
@@ -5,21 +5,17 @@ import { runSearchPage } from "./pages/searchPage.js";
 
 // Om index.html-sidan laddas körs detta
 if(window.location.pathname === '/' || window.location.pathname === '/index.html') {
-    console.log('index.html');
     runIndexPage();
     
     // Om favorites.html laddas körs detta
 } else if(window.location.pathname === '/favorites.html') {
-    console.log('favorites.html');
     runFavoritesPage();
     
     // Om movie.html laddas körs detta
 } else if(window.location.pathname === '/movie.html') {
-    console.log('movie.html');
     runMoviePage();
     
     // Om search.html laddas körs detta
 } else if(window.location.pathname === '/search.html') {
-    console.log('search.html');
     runSearchPage();
 };
