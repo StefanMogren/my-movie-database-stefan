cc95399d62e44f4c5603f93ace242ad356783813
StefanMogren <stefan_mogren@hotmail.com>
2025-02-28 10:12:32 +0100

    Moved URLSearchParams and associated code from script.js to runSeachPage function in searchPage.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,12 @@
-import { searchOMDbMoviesAPI } from "./modules/api.js";
-import { runMoviePage } from "./components/moviePage.js";
-import { runFavoritesPage } from "./components/favoritesPage.js"
-import { runSearchPage } from "./components/searchPage.js";
-import { oData } from "./data/data.js";
 import { runIndexPage } from "./components/indexPage.js";
+import { runFavoritesPage } from "./components/favoritesPage.js";
+import { runMoviePage } from "./components/moviePage.js";
+import { runSearchPage } from "./components/searchPage.js";
 
 // Om index.html-sidan laddas körs detta
 if(window.location.pathname === '/' || window.location.pathname === '/index.html') {
     console.log('index.html');
     runIndexPage();
-    
     
     // Om favorites.html laddas körs detta
 } else if(window.location.pathname === '/favorites.html') {
@@ -21,23 +18,8 @@ if(window.location.pathname === '/' || window.location.pathname === '/index.html
     console.log('movie.html');
     runMoviePage();
     
-    
-    
     // Om search.html laddas körs detta
 } else if(window.location.pathname === '/search.html') {
-    window.onload = async () => {
-        console.log('search.html');
-        const urlParams = new URLSearchParams(window.location.search);
-        oData.searchInput = urlParams.get("searchMovie")
-        
-        if(oData.searchInput) {
-            await searchOMDbMoviesAPI(apiKeyOMDb, oData.searchInput)
-            runSearchPage();
-        } else {
-            console.error("No search input is present...");
-        }
-        
-    }
-    
-}
-
+    console.log('search.html');
+    runSearchPage();
+};
